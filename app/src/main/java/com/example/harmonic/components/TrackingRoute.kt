28d52a43522876ac7032658bbee207722c9b2ce2
Devoxys <davekpeng@gmail.com>
2024-02-21 16:09:46 -0500

    select tracking jobs menu

---
+++ app/src/main/java/com/example/harmonic/components/TrackingRoute.kt
@@ -0,0 +1,18 @@
+package com.example.harmonic.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TrackingRoute(
+    onGoToTimerJob: () -> Unit,
+    onGoToRoutineJob: () -> Unit,
+    onGoToCounterJob: () -> Unit,
+    onGoToDecimalJob: () -> Unit
+) {
+    TrackingScreen(
+        onGoToTimerJobs = { onGoToTimerJob() },
+        onGoToRoutineJobs = { onGoToRoutineJob() },
+        onGoToCounterJobs = { onGoToCounterJob() },
+        onGoToDecimalJobs = { onGoToDecimalJob() }
+    )
+}
