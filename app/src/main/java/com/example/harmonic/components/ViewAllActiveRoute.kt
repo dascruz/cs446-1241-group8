cea5751328a399522ef1c0c9792b53b5ff93de16
Devoxys <davekpeng@gmail.com>
2024-02-29 22:05:44 -0500

    created ViewAllActiveRoute.kt

---
+++ app/src/main/java/com/example/harmonic/components/ViewAllActiveRoute.kt
@@ -0,0 +1,14 @@
+package com.example.harmonic.components
+
+import androidx.compose.runtime.Composable
+import java.util.UUID
+
+@Composable
+fun ViewAllActiveRoute(
+    onNavigateToActiveTimerInstance: (id: UUID) -> Unit,
+    onNavigateToActiveRoutineInstance: (id: UUID) -> Unit,
+    onNavigateToActiveDecimalInstance: (id: UUID) -> Unit,
+    onNavigateToActiveCounterInstance: (id: UUID) -> Unit
+) {
+
+}
